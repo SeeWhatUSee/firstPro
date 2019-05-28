92b6be07f5651e7ae6afd758d5fda133ae3fbf6d
liwei <seewhatusee@163.com>
2019-05-28 15:01:31 +0800

    add gitTest.java

---
+++ GitTest.java
@@ -0,0 +1,20 @@
+public class GitTest{
+	private String operation;
+	private String userName;
+	public GitTest(String operation,String userName){
+		this.operation = operation;
+		this.userName = userName;
+	}
+	public void setOperation(String operation){
+		this.operation = operation;
+	}
+	public String getOperation(){
+		return this.operation
+	}
+	public void setUserName(String userName){
+		this.userName = userName;
+	}
+	public String getUserName(){
+		return this.userName
+	}
+}
